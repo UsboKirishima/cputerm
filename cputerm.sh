845e6aca02a3594769197bc41c0e209a84f163ba
UsboKirishima <usbertibox@gmail>
2022-06-13 12:51:27 +0000

    feat: add copyright and datalist

---
+++ cputerm.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+#-Metadata----------------------------------------------------#
+#  Filename: cputermv1.0.0)           (Update: 2022-06-13)    #
+#-Info--------------------------------------------------------#
+#  cputerm - CPU terminal viewer                              #
+#-Author(s)---------------------------------------------------#
+#  Davide Usberti ~ @UsboKirishima                            #
+#-Operating System--------------------------------------------#
+#  Designed for & tested on: Linux 	                      #
+#  Reported working : Ubuntu 16                               #
+#	   	    : Parrot 				      #
+#	   	    : BlackArch				      #
+#                   : Kali Linux 			      #
+#		    : Backbox     			      #
+#  Available on BlackArch Linux                               #
+#-Licence-----------------------------------------------------#
+#  MIT License                                                #
+#-------------------------------------------------------------#
+
+
+
+
